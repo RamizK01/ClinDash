ef2c3faeb38ba2da7673c4fee11aef8f2fde11dc
RamizK01 <ramizk2001@gmail.com>
2025-07-06 22:26:38 -0400

    develop basic app and format utils

--- app.py
+++ app.py
@@ -1,11 +1,62 @@
-from dash import Dash, html
+from dash import Dash, html, dcc, dash_table, Input, Output, State
+from utils import get_studies, process_studies
+import pandas as pd
 
-app = Dash() 
+app = Dash()
 
-app.layout = html.Div([
-    html.H1("Hello Dash"),
-    html.Div("Dash: A web application framework for Python."),
-    html.Div("This is a simple Dash app.")])
+app.layout = html.Div(
+    [
+        html.H1(
+            "ClinDash",
+            style={"textAlign": "center", "color": "#0074D9", "fontFamily": "Roboto"},
+        ),
+        html.Div(
+            [
+                "Open-Source Clinical Trials Dashboard using available records from ",
+                html.A(
+                    "Clinicaltrials.gov",
+                    href="https://clinicaltrials.gov/",
+                    target="_blank",
+                    style={
+                        "color": "#2813E0",
+                        "textDecoration": "underline",
+                        "fontFamily": "Roboto",
+                    },
+                ),
+            ],
+            style={"textAlign": "center", "color": "#000000", "fontFamily": "Roboto"},
+        ),
+        dcc.Input(
+            id="usr_searchexpr", placeholder="Search Expression", type="text", value=""
+        ),
+        html.Button("Go", id="submit-btn", n_clicks=0),
+        html.Div(id="results_div"),
+    ]
+)
 
-if __name__ == '__main__':
+
+@app.callback(
+    Output("results_div", "children"),
+    Input("submit-btn", "n_clicks"),
+    State("usr_searchexpr", "value"),
+)
+def run_search(n_clicks, search_expr):
+    if n_clicks == 0:
+        return ""
+    if not search_expr:
+        return "Please enter a search expression."
+
+    api_result = get_studies(search_expr)
+    df = process_studies(api_result, type="general")
+
+    table = dash_table.DataTable(
+        id="results_div",
+        columns=[{"name": i, "id": i} for i in df.columns],
+        data=df.to_dict("records"),
+    )
+
+    return table
+
+
+if __name__ == "__main__":
     app.run(debug=True)
--- utils.py
+++ utils.py
@@ -5,104 +5,115 @@ import pandas as pd
 
 
 def get_studies(search_expr):
-    '''
+    """
     Pulls studies from ctgov API based on user input search expression.
 
     Ensure that the search expression is not too generic so that the API does not crash.
 
     Expressions like "Cancer" are generic. Be more specific such as "Head OR Neck AND Cancer".
-    '''
+    """
     base_url = "https://clinicaltrials.gov/api/v2/studies"
-    params = {
-        "pageSize": 500, 
-        "query.term": search_expr  
-    }    
-    
+    params = {"pageSize": 500, "query.term": search_expr}
+
     all_studies = []
     # Set page token to iterate through >500 studies for search expresion
     next_page_token = None
-    
+
     while True:
         # Set limit @ 10 000 studies before API gets killed
         if len(all_studies) > 10000:
             ValueError("Search Expression is too generic, please specify the query")
-        
+
         if next_page_token:
             params["pageToken"] = next_page_token
-        
+
         response = requests.get(base_url, params=params)
         data = response.json()
-        
+
         all_studies.extend(data.get("studies", []))
         next_page_token = data.get("nextPageToken")
 
         if not next_page_token:
             break
-    
+
     # returns as a json-type list
-    return(all_studies)
+    return all_studies
+
+
+class trials:
+    def __init__(self, search_expr):
+        """
+        Initialize the trials class with a search expression.
+        """
+        self.search_expr = search_expr
+        self.studies = get_studies(search_expr)
 
 
 def process_studies(studies, type):
-    '''
+    """
     Process the studies to extract relevant information and convert to a pandas DataFrame.
     Specify the type of data to extract: "general" or "location".
-    '''
+    """
     data = pd.json_normalize(studies)
 
     if type == "general":
-        study_df = data[[
-            "protocolSection.identificationModule.nctId",
-            "protocolSection.identificationModule.organization.fullName",
-            "protocolSection.sponsorCollaboratorsModule.responsibleParty.type",	
-            "protocolSection.identificationModule.organization.class",
-            "protocolSection.identificationModule.briefTitle",
-            "protocolSection.identificationModule.officialTitle",
-            "protocolSection.statusModule.overallStatus",
-            "protocolSection.statusModule.lastKnownStatus",
-            "protocolSection.statusModule.whyStopped",
-            "protocolSection.descriptionModule.briefSummary",
-            "protocolSection.descriptionModule.detailedDescription",
-            "protocolSection.designModule.studyType",
-            "protocolSection.designModule.phases",
-            "protocolSection.designModule.designInfo.allocation",
-            "protocolSection.designModule.designInfo.primaryPurpose",
-            "protocolSection.designModule.designInfo.interventionModel",
-            "protocolSection.designModule.enrollmentInfo.count",
-            "protocolSection.designModule.enrollmentInfo.type",
-            "protocolSection.eligibilityModule.healthyVolunteers",
-            "protocolSection.eligibilityModule.sex",
-            "protocolSection.eligibilityModule.minimumAge",
-            "protocolSection.eligibilityModule.maximumAge",
-            "protocolSection.eligibilityModule.stdAges",
-            "protocolSection.statusModule.startDateStruct.date",
-            "protocolSection.statusModule.primaryCompletionDateStruct.date",  
-            "protocolSection.statusModule.completionDateStruct.date"
-            ]]
-        
-        return(study_df)
-    
+        study_df = data[
+            [
+                "protocolSection.identificationModule.nctId",
+                "protocolSection.identificationModule.organization.fullName",
+                "protocolSection.sponsorCollaboratorsModule.responsibleParty.type",  #
+                "protocolSection.identificationModule.organization.class",  #
+                "protocolSection.identificationModule.briefTitle",  #
+                "protocolSection.identificationModule.officialTitle",
+                "protocolSection.statusModule.overallStatus",
+                "protocolSection.statusModule.lastKnownStatus",
+                "protocolSection.statusModule.whyStopped",  #
+                "protocolSection.descriptionModule.briefSummary",  #
+                "protocolSection.descriptionModule.detailedDescription",  #
+                "protocolSection.designModule.studyType",
+                "protocolSection.designModule.phases",
+                "protocolSection.designModule.designInfo.allocation",
+                "protocolSection.designModule.designInfo.primaryPurpose",
+                "protocolSection.designModule.designInfo.interventionModel",
+                "protocolSection.designModule.enrollmentInfo.count",
+                "protocolSection.designModule.enrollmentInfo.type",
+                "protocolSection.eligibilityModule.sex",  #
+                "protocolSection.eligibilityModule.minimumAge",  #
+                "protocolSection.eligibilityModule.maximumAge",  #
+                "protocolSection.eligibilityModule.stdAges",  #
+                "protocolSection.statusModule.startDateStruct.date",
+                "protocolSection.statusModule.primaryCompletionDateStruct.date",  #
+                "protocolSection.statusModule.completionDateStruct.date",
+            ]
+        ]
+
+        def flatten_data(df):
+            # Convert nested columns (dicts/lists) to JSON strings
+            for col in df.columns:
+                if df[col].apply(lambda x: isinstance(x, (dict, list))).any():
+                    df[col] = df[col].apply(lambda x: json.dumps(x) if x else "")
+            return df
+
+        study_df = flatten_data(study_df)
+
+        return study_df
+
     elif type == "location":
         temp_loc_df = data[["protocolSection.contactsLocationsModule.locations"]]
 
-        for i, study in enumerate(temp_loc_df["protocolSection.contactsLocationsModule.locations"]):
-            # remove empty entries 
+        for i, study in enumerate(
+            temp_loc_df["protocolSection.contactsLocationsModule.locations"]
+        ):
+            # remove empty entries
             if isinstance(study, float):
                 continue
-            
+
             if i == 0:
                 loc_df = pd.json_normalize(study)
 
             loc_df = pd.concat([loc_df, pd.json_normalize(study)], ignore_index=True)
 
-        return(loc_df)
-    
+        return loc_df
+
     else:
         raise ValueError("Invalid type. Use 'general' or 'location'.")
-
-
-
-
-
-
-
